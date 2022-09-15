e975f2ee9ca955c1077400300356037fba7f8421
Crybaby07 <105725202+Crybaby07@users.noreply.github.com>
2022-09-15 14:10:23 +0600

    Countdown- fixed  secound count fixed-

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
@@ -115,6 +115,8 @@ const start = () => {
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
       display.classList.remove("inactive");
+
+      countdownOverlay.style.display = "none";
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
@@ -135,5 +137,5 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
